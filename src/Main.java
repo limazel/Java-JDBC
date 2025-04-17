c8b4431e25c85940155174121746d8b5fed1b5d4
Azad Ayhan <azadayhan50@gmail.com>
2025-04-17 11:07:51 +0300

    Workng with SQL

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
   
@@ -7,9 +9,18 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         Connection connection = null;
         DbHelper helper = new DbHelper();
+        Statement statement = null;
+        ResultSet resultSet = null;
         try {
             connection = helper.getConnection();
-            System.out.println("Connection established successfully.");
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM students");
+            while(resultSet.next()) {
+                System.out.println("Student ID: " + resultSet.getInt("id"));
+                System.out.println("Student Name: " + resultSet.getString("name"));
+                System.out.println("Student Age: " + resultSet.getInt("age"));
+                System.out.println("------------------------------");
+            }
         } catch (SQLException exception) {
             helper.showErrorMessage(exception);
         } catch (Exception exception) {
